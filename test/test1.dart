f2901fb4f189585e9162c4b96e7e9e62f9d299ee
Oliver Damm <oliver.damm@gmx.de>
2022-10-17 23:40:24 +0200

    small changes

--- test/test1.dart
+++ test/test1.dart
@@ -9,11 +9,6 @@ void main() async {
         '[${record.time.toString().padRight(26, '0')}|${record.level.name.padLeft(7, ' ')}|${record.loggerName.abbreviate(20).padLeft(20)}] ${record.message}');
   });
 
-  final node = await SystemNode('config1')
+  await SystemNode('config2')
     ..init();
-
-  final state = node.state;
-  if (state == NodeState.started) {
-    print('Node is running');
-  }
 }
